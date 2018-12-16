bbcd597b77dd8fcdad8f83c787da5f01d1fec3cf
490158859 <490158859@qq.com>
2018-12-16 21:42:18 +0800

    1111

---
+++ Chat/PersonHub.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using Newtonsoft.Json;
+
+namespace WebApplication1
+{
+
+    [HubName("MyPersonHub")]
+    public class PersonHub : Hub
+    {
+        public void Hello(string msg)
+        {
+            //欢迎方法
+            Clients.All.Welcome("very good");
+        }
+
+        [HubMethodName("MyHello")]
+        public void Hello(int msg)
+        {
+            Clients.All.Welecome("我是字符串");
+        }
+
+        public async Task GetDate()
+        {
+           await Clients.All.GetDate(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+        }
+        public async Task SenMsg(string message)
+        {
+            var data = new { Name = Context.ConnectionId, msg = message, date = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") };
+            string msgBody = JsonConvert.SerializeObject(data);
+            await Clients.All.GetMsg(data);
+        }
+        /*  在下面的三个方法中，我们可以用来维护 connectionID 列表  */
+        public override Task OnConnected()
+        {
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            return base.OnDisconnected(stopCalled);
+        }
+
+        public override Task OnReconnected()
+        {
+            return base.OnReconnected();
+        }
+    }
+}
---
+++ Chat/Startup1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebApplication1.Startup1))]
+
+namespace WebApplication1
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //Data Source=.;Initial Catalog=brnmall;User ID=sa
+            //GlobalHost.DependencyResolver.UseSqlServer("Data Source=.;Initial Catalog=RbCms;Persist Security Info=True;User ID=sa;Password=123456");
+            GlobalHost.DependencyResolver.UseRedis("localhost", 6379, string.Empty, "mykey");
+            // 有关如何配置应用程序的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkID=316888
+            app.MapSignalR("/myhub", new HubConfiguration()
+            { EnableDetailedErrors=true});
+        }
+    }
+}
