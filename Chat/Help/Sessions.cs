628419ee660447c6d0cf2a690a80b77d645662c8
490158859 <490158859@qq.com>
2018-12-18 12:09:22 +0800

    1

---
+++ Chat/Help/Sessions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Chat.Help
+{
+    public class Sessions
+    {
+        /// <summary>
+        /// 生成sessionid
+        /// </summary>
+        /// <returns></returns>
+        public static string GenerateSid()
+        {
+            long i = 1;
+            byte[] byteArray = Guid.NewGuid().ToByteArray();
+            foreach (byte b in byteArray)
+            {
+                i *= ((int)b + 1);
+            }
+            return string.Format("{0:x}", i - DateTime.Now.Ticks);
+        }
+    }
+}
